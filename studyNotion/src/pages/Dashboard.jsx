8fa14f6734ba50e2a629da7c557146e408f2a6a6
imshree03 <shreebhosale24hrs@gmail.com>
2024-03-18 20:25:34 +0530

    Basic Dashboard

---
+++ studyNotion/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center  h-[100vh] w-[100vw] text-size-300">
+      Welcome to Study Notion!
+    </div>
+  );
+};
+
+export default Dashboard;
